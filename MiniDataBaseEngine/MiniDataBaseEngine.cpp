0c476a9b558d3689530e76b6e4a3e3ad14fc4ef4
Emanuel Jose <emanueljose0522@hotmail.com>
2026-02-09 19:47:01 -0500

    finished the basic functionality for a working RDBMS

--- MiniDataBaseEngine/MiniDataBaseEngine.cpp
+++ MiniDataBaseEngine/MiniDataBaseEngine.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <map>
+#include <string>
+#include <sstream>
 #include "Table.h"
 #include <vector>
+#include <cctype>
 
 
 class MiniDb
@@ -33,31 +36,133 @@ public:
         std::cout << "Using table '" << name << "'.\n";
     }
 
+    void insertRow(const std::vector<std::string>& values)
+    {
+        if (!current)
+        {
+            std::cout << "no to table selected\n";
+            return;
+        }
+
+        if (values.size() != current->columns.size())
+        {
+            std::cout << "columns do not match";
+            return;
+        }
+        for (size_t i = 0; i < values.size(); i++)
+        {
+            if (current->columns[i].type == INT)
+            {
+                for (char c : values[i])
+                {
+                    if(!isdigit(c))
+                    {
+                        std::cout << "invalid int value\n";
+                        return;
+                    }
+                }
+            }
+        }
+
+        Row r;
+        r.values = values;
+        current->rows.push_back(r);
+        current->save();
+        std::cout << "Row inserted\n";
+    }
+
+    void selectAll()
+    {
+        if (!current)
+        {
+            std::cout << "No Table selected.\n";
+            return;
+        }
+
+        for (auto& c : current->columns)
+        {
+            std::cout << c.name << "\t";
+        }
+        std::cout << "\n";
+
+        for (auto& r : current->rows)
+        {
+            for (auto& v : r.values)
+            {
+                std::cout << v << "\t";
+            }
+            std::cout << "\n";
+        }
+    }
+
     void run()
     {
         std::string line;
         while (true)
         {
-            std::cout << "db->";
+            std::cout << "db > ";
             std::getline(std::cin, line);
 
-            if (line == "exit") break;
+            if (line == "EXIT") break;
 
             else if (line.find("CREATE") == 0)
             {
-                std::cout << "creating a table ...\n";
-                //auto p1 = line.find("(");
-                //auto p2 = line.find(")");
+                //std::cout << "creating a table ...\n";
+                auto p1 = line.find("(");
+                auto p2 = line.find(")");
 
-                //std::string name = line.substr(7, p1 - 7);
-                //name.erase(name.find_last_not_of(" ") + 1);
+                std::string name = line.substr(7, p1 - 7);
+                name.erase(name.find_last_not_of(" ") + 1);  //if the last char is a space erase it
+
+                std::string cols = line.substr(p1 + 1, p2 - p1 - 1);
+                std::stringstream ss(cols);
+                std::vector<Column> columns;
+
+                std::string part;
+                while (std::getline(ss, part, ','))
+                {
+                    std::stringstream ps(part);
+                    Column c;
+                    std::string type;
+                    ps >> c.name >> type;
+                    c.type = (type == "INT") ? INT : TEXT;
+                    columns.push_back(c);
+                }
+                createTable(name, columns);
 
 
             }
             else if (line.find("USE") == 0)
             {
-                std::cout << "using table <table_name> ... \n";
+                std::string name = line.substr(4);
+                useTable(name);
             }
+            else if (line.find("INSERT") == 0)
+            {
+                auto p1 = line.find("(");
+                auto p2 = line.find(")");
+
+                std::string vals = line.substr(p1 + 1, p2 - p1 - 1);
+                std::stringstream ss(vals);
+                std::vector<std::string> values;
+                std::string v;
+                while (std::getline(ss, v, ','))
+                {
+                    v.erase(0, v.find_first_not_of(" "));
+                    values.push_back(v);
+                }
+                insertRow(values);
+            }
+
+            else if (line == "SELECT *")
+            {
+                selectAll();
+            }
+            else
+            {
+                std::cout << "Invalid command.\n";
+            }
+
 
 
         }
